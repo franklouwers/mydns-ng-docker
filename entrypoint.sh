360b0d2c308a57726c22ed5164886a7b1dcb61c4
Frank Louwers <frank@louwers.be>
2019-03-07 12:48:42 +0100

    Allow loading an sql dump on start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 /usr/bin/mysqld_safe --nowatch
 sleep 5
+
+# If /load.sql exists, load that dump into the mydns Database
+[ -f /load.sql ] && mysql mydns < /load.sql
+
 /usr/local/sbin/mydns
 
